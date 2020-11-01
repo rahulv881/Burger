054cc2c69392d5d481c87593db8716c4e662320c
rahul <rahulv881@gmail.com>
2020-11-01 19:06:59 +0530

    Lecture141 #OrderSummary Button Missing

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,13 +13,22 @@ const controls = [
 const buildControls = (props) =>{
     return (
       <div className={classes.BuildControls}>
-        {controls.map((ctrl) => (
+      <p><strong>Current Price: {props.price.toFixed(2)}</strong></p>
+        {
+          controls.map((ctrl) => (
           <BuildControl
             key={ctrl.lable}
             lable={ctrl.lable}
             added={() => props.ingredientAdded(ctrl.type)}
+            removed={() => props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}
           />
         ))}
+        <button 
+          className={classes.OrderButton} 
+          disabled={!props.purchasable} 
+          onClick={props.ordered}
+        >ORDER NOW</button>
       </div>
     );
 };
