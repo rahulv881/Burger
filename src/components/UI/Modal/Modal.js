759ba3ee8572aa88492d359a289bfbd18107767a
rahul <rahulv881@gmail.com>
2020-11-06 18:09:30 +0530

    OrderSummary transfrom spelling error fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,7 @@ const modal = (props) => (
     <div
       className={classes.Modal}
       style={{
-        transfrom: props.show ? "translateY(0)" : "translateY(-100vh)",
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
         opacity: props.show ? "1" : "0",
       }}
     >
